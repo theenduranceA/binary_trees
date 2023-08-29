33bb6e4770a4bbf42df7e2b952bb41854e9848c4
theenduranceA <theenduranceaneke@gmail.com>
2023-08-29 06:22:23 -0700

    Task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,7 +12,7 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *new_node = NULL;
 
 	if (parent == NULL)
 		return (NULL);
@@ -20,6 +20,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->parent = parent;
 	new_node->n = value;
 
