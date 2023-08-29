2036a48512f2571b9c6555a8b20e92c20265e108
theenduranceA <theenduranceaneke@gmail.com>
2023-08-29 06:38:54 -0700

    Task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,7 +12,7 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = NULL;
+	binary_tree_t *new_node;
 
 	if (parent == NULL)
 		return (NULL);
@@ -21,16 +21,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->parent = parent;
 	new_node->n = value;
+	new_node->left = parent->left;
+	new_node->right = NULL;
 
-	if (parent->left != NULL)
+	if (parent->left)
 	{
-		new_node->left = parent->left;
-		new_node->left->parent = new_node;
+		parent->left->parent = new_node;
 	}
+
+	new_node->parent = parent;
 	parent->left = new_node;
 
 	return (new_node);
 }
-
