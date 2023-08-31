ebfb78984ca1c800c6768d134eac529156c65ece
theenduranceA <theenduranceaneke@gmail.com>
2023-08-31 07:41:11 -0700

    Task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -21,9 +21,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	right = binary_tree_height(tree->right);
 
 	if (left == right &&
-	binary_tree_is_perfect(tree->left) &&
-	binary_tree_is_perfect(tree->right))
-
+		binary_tree_is_perfect(tree->left) &&
+		binary_tree_is_perfect(tree->right))
 		return (1);
 
 	return (0);
